868f634f38b246b8cce0d97beed3d250fa814fb3
long.zhang <long.zhang@ele.me>
2015-04-28 12:06:38 +0800

    Export Popup to global.

--- src/export.js
+++ src/export.js
@@ -1,16 +1,24 @@
-var NAME = 'Popover';
+var POPUP_NAME = 'Popup';
+var POPOVER_NAME = 'Popover';
 
+var Popup = require('./popup');
 var Popover = require('./popover');
 
 if (typeof define === 'function' && define.amd) { // For AMD
-  define(function() {
+  define(POPUP_NAME, function() {
+    return Popup;
+  });
+  define(POPOVER_NAME, function() {
     return Popover;
   });
 } else if (typeof angular === 'object' && !!angular.version) {
-  angular.module('popover.js', []).factory(NAME, function() {
+  angular.module('popover.js', []).factory(POPUP_NAME, function() {
+    return Popup;
+  }).factory(POPOVER_NAME, function() {
     return Popover;
   });
 } else {
-  Number(document.documentMode) < 9 && window.execScript('var ' + NAME);
-  window[NAME] = Popover;
+  Number(document.documentMode) < 9 && window.execScript('var ' + POPUP_NAME + ',' + POPOVER_NAME + ';');
+  window[POPUP_NAME] = Popup;
+  window[POPOVER_NAME] = Popover;
 }
